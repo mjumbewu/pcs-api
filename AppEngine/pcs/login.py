5015f76a45f27791381f18786e85c20989e73028
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2010-08-11 15:21:09 -0400

    Revert "Revert "Some changes that should have made it into the last commit.""
    This reverts commit 9496acb280e60eaccee96a61a6163ee036ba5453.

--- AppEngine/pcs/login.py
+++ AppEngine/pcs/login.py
@@ -7,58 +7,8 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from pcs.data.session import Session
+from pcs.view.login import LoginHtmlView
 
-class LoginHtmlView (object):
-    @staticmethod
-    def FailureResponse(username):
-        return """
-<!DOCTYPE html>
-
-<html>
-  <head>
-    <title>Login Failed</title>
-  </head>
-  
-  <body>
-    <h1>Login Failed</h1>
-    <p>Check your username and password and try again</p>
-    <form action="/login" method="POST">
-      <label for="username">User ID:</label>
-      <input type="text" value="%s" name="username" />
-      <label for="password">Password:</label>
-      <input type="password" name="password" />
-      <input type="submit" />
-    </form>
-  </body>
-</html>
-        """ % (username)
-        
-    @staticmethod
-    def SuccessResponse(session):
-        return """
-<!DOCTYPE html>
-
-<html>
-  <head>
-    <title>Logged In</title>
-  </head>
-  
-  <body>
-    <h1>Welcome, %s</h1>
-    <p><a href="%s">Make a New Reservation</a></p>
-    <p><a href="%s">Manage Existing Reservations</a></p>
-    <p><a href="%s">View Messages</a></p>
-    <p><a href="%s">Manage Account Info</a></p>
-    <p><a href="%s">Give Feedback</a></p>
-  </body>
-</html>
-          """ % (session.name,
-                 session.get_new_reservation_url(),
-                 session.get_existing_reservations_url(),
-                 session.get_messages_url(),
-                 session.get_account_info_url(),
-                 session.get_feedback_url())
-    
 class LoginHandler (webapp.RequestHandler):
     """
     Handles requests sent to the list of products
