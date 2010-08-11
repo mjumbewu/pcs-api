ae1304f04c0fb38bdc65c079443732af6c041d34
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2010-08-11 18:39:50 -0400

    refactored login screenscraper so that the connection is passed into the login method.  this is to facilitate testing.  it's not yet ideal, though, as the connection is still created in the get_new_session method.  however, it works out that this doesn't matter, in practice.

--- AppEngine/pcs/source/screenscrape/login.py
+++ AppEngine/pcs/source/screenscrape/login.py
@@ -10,20 +10,22 @@ class LoginScreenscrapeSource (object):
     Responsible for logging in and constructing a session from a screenscrape
     of a PhillyCarShare response.
     """
+    SIMPLE_FAILURE_DOCUMENT = "<html><head><title>Please Login</title></head><body></body></html>"
+    
     def __init__(self, host="reservations.phillycarshare.org",
                  path="/my_reservations.php"):
         super(LoginScreenscrapeSource, self).__init__()
         self.__host = host
         self.__path = path
     
-    def login_to_pcs(self, username, password):
+    def login_to_pcs(self, conn, username, password):
         """
-        Attempts to login with the given username and password.
+        Attempts to login to the given connection with the given username and 
+        password.
+        
         @return: The server response
         @raise: DownloadError if getting response from the server fails.
         """
-        conn = httplib.HTTPSConnection(self.__host)
-        
         parameters = urllib.urlencode({
             'login[name]': username,
             'login[password]': password})
@@ -34,9 +36,9 @@ class LoginScreenscrapeSource (object):
         try:
             response = conn.getresponse()
         except:
-            return "<html><head><title>Please Login</title></head><body></body></html>", []
+            return (self.SIMPLE_FAILURE_DOCUMENT, [])
         
-        return response.read(), response.getheaders()
+        return (response.read(), response.getheaders())
     
     def login_was_successful(self, response_body):
         """
@@ -76,7 +78,10 @@ class LoginScreenscrapeSource (object):
             raise Exception('Unknown Login Title: %r' % parser.title)
     
     def get_new_session(self, username, password):
-        pcs_login_body, pcs_login_headers = self.login_to_pcs(username, password)
+        
+        conn = httplib.HTTPSConnection(self.__host)
+        
+        pcs_login_body, pcs_login_headers = self.login_to_pcs(conn, username, password)
         self._body = pcs_login_body
         self._headers = pcs_login_headers
         
--- AppEngine/test/test_login.py
+++ AppEngine/test/test_login.py
@@ -17,7 +17,7 @@ class login_screenscrape_TestCase (unittest.TestCase):
     def test3(self):
         """Expected session is returned from a failed login (expected session is None)."""
         source = LoginScreenscrapeSource()
-        source.login_to_pcs = lambda username, password: \
+        source.login_to_pcs = lambda conn, username, password: \
             ("<html><head><title>Please Login</title></head><body></body></html>", {})
         session = source.get_new_session('uid', 'pass')
         self.assert_(session is None)
@@ -25,7 +25,7 @@ class login_screenscrape_TestCase (unittest.TestCase):
     def test4(self):
         """Expected session is returned from a successful login."""
         source = LoginScreenscrapeSource()
-        source.login_to_pcs = lambda username, password: \
+        source.login_to_pcs = lambda conn, username, password: \
             ("""<html>
                   <head>
                     <title>My Message Manager</title>
@@ -40,4 +40,28 @@ class login_screenscrape_TestCase (unittest.TestCase):
         self.assertEqual(session.id, 'abcde')
         self.assertEqual(session.user, 'uid')
         self.assertEqual(session.name, 'Mr. Bojangles')
+    
+    def test5(self):
+        """Screenscrape source should have a simple document that triggers login failure"""
+        source = LoginScreenscrapeSource()
+        document = LoginScreenscrapeSource.SIMPLE_FAILURE_DOCUMENT
+        self.assert_(not source.login_was_successful(document))
+    
+    def test6(self):
+        """Logging in will get response from connection"""
+        source = LoginScreenscrapeSource()
+        
+        class StubConnection (object):
+            def request(self, method, path, data='', headers={}):
+                pass
+            def getresponse(self):
+                from StringIO import StringIO
+                response = StringIO('Body Text')
+                response.getheaders = lambda: {'h1':1,'h2':2}
+                return response
+        
+        conn = StubConnection()
+        body, headers = source.login_to_pcs(conn, 'uid', 'pass')
+        self.assertEqual(body, 'Body Text')
+        self.assertEqual(headers, {'h1':1,'h2':2})
     
